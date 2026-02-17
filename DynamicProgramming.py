3df30839e47912914b61ff574af30a8c534d55d7
Ishwari1316188 <veerisha123@gmail.com>
2026-02-17 13:08:43 +0530

    Update DynamicProgramming.py
    Edited the first part of code

--- DynamicProgramming.py
+++ DynamicProgramming.py
@@ -2,9 +2,13 @@ import tkinter as tk
 from tkinter import messagebox
 from PIL import Image, ImageTk
 from copy import deepcopy
-from collections import deque
 import random
+from collections import deque
 import os
+
+# PATH SETUP 
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 # CONSTANTS 
 
@@ -12,60 +16,36 @@ TILE = 160
 BOARD_SIZE = 480
 BG_COLOR = "misty rose"
 
-GOAL = [[1, 2, 3],
-        [4, 5, 6],
-        [7, 8, 0]]
-
+GOAL_STATE = (1, 2, 3, 4, 5, 6, 7, 8, 0)
 DIRS = [(-1, 0), (1, 0), (0, -1), (0, 1)]
 
-# DP (BFS) SOLVER 
+#  DP TABLE 
 
-def state_to_string(state):
-    return ''.join(str(cell) for row in state for cell in row)
+dp_table = {}
 
-def string_to_state(s):
-    nums = list(map(int, s))
-    return [nums[i:i+3] for i in range(0, 9, 3)]
 
-def find_zero(state):
-    for r in range(3):
-        for c in range(3):
-            if state[r][c] == 0:
-                return r, c
+def build_dp_table():
+    if dp_table:
+        return
 
-# GAMEPLAY
+    queue = deque([GOAL_STATE])
+    dp_table[GOAL_STATE] = None
 
-def move_user(self, r, c):
-zr, zc = find_zero(self.user_state)
-if abs(r - zr) + abs(c - zc) == 1:
-    self.user_state[zr][zc], self.user_state[r][c] = \
-        self.user_state[r][c], 0
-    self.user_steps += 1
-    self.update_boards()
+    while queue:
+        curr = queue.popleft()
 
-    if self.user_state == GOAL:
-        messagebox.showinfo(
-            "User Solved",
-            f"You solved it in {self.user_steps} steps!")
+        idx = curr.index(0)
+        r, c = divmod(idx, 3)
 
-def shuffle(self):
-state = deepcopy(GOAL)
-for _ in range(150):
-    zr, zc = find_zero(state)
-    dr, dc = random.choice(DIRS)
-    nr, nc = zr + dr, zc + dc
-    if 0 <= nr < 3 and 0 <= nc < 3:
-        state[zr][zc], state[nr][nc] = state[nr][nc], 0
+        for dr, dc in DIRS:
+            nr, nc = r + dr, c + dc
+            if 0 <= nr < 3 and 0 <= nc < 3:
+                ni = nr * 3 + nc
+                nxt = list(curr)
+                nxt[idx], nxt[ni] = nxt[ni], nxt[idx]
+                nxt = tuple(nxt)
 
-self.user_state = deepcopy(state)
-self.ai_state = deepcopy(state)
-self.user_steps = 0
-self.ai_steps = 0
-self.update_boards()
+                if nxt not in dp_table:
+                    dp_table[nxt] = curr
+                    queue.append(nxt)
 
-# DP MACHINE
-
-def solve_ai(self):
-self.ai_steps = 0
-path = solve_with_dp(deepcopy(self.ai_state))
-self.animate_ai(path, 0)
